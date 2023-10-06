735a625f5e36d35e9e9fcb70b6aaf16a3cb7ca17
MartinH13 <mhorsfield001@ikasle.ehu.eus>
2023-10-06 09:19:46 +0200

    mockito en comentario

--- src/test/java/test/dataAccess/BetsMockitoTest.java
+++ src/test/java/test/dataAccess/BetsMockitoTest.java
@@ -5,7 +5,8 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import org.mockito.Mockito;
 
-
+/*
 public class BetsMockitoTest {
 	ForumDAOInterface forumDAO= Mockito.mock(DataAccess.class);
 }
+*/
