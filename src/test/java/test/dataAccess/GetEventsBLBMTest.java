6fcaccecde37992bfbd2642c0c238ea1854ea7e4
MartinH13 <martin.i.horsfield@gmail.com>
2023-10-12 00:20:10 +0200

    prueba

--- src/test/java/test/dataAccess/GetEventsBLBMTest.java
+++ src/test/java/test/dataAccess/GetEventsBLBMTest.java
@@ -1,5 +1,22 @@
 package test.dataAccess;
 
-public class GetEventsBLBMTest {
+import static org.junit.Assert.*;
 
+import java.security.NoSuchAlgorithmException;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.junit.*;
+import org.mockito.*;
+
+import businessLogic.BLFacadeImplementation;
+import configuration.UtilDate;
+import domain.*;
+import exceptions.EventFinished;
+import exceptions.NotEnoughMoneyException;
+import exceptions.ObjectAlreadyExistException;
+import dataAccess.DataAccess;
+
+public class GetEventsBLBMTest {
+	
 }
