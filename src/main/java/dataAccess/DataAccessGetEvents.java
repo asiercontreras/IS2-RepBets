60d406e54050d0358f6d4dcfd6e5f3d9b046ef12
MartinH13 <martin.i.horsfield@gmail.com>
2023-10-19 16:36:07 +0200

    formulario pruebas martin

---
+++ src/main/java/dataAccess/DataAccessGetEvents.java
@@ -0,0 +1,90 @@
+package dataAccess;
+
+//hello
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+import configuration.ConfigXML;
+import domain.*;
+
+/**
+ * It implements the data access to the objectDb database
+ */
+public class DataAccessGetEvents {
+	protected EntityManager db;
+	protected EntityManagerFactory emf;
+
+	ConfigXML c = ConfigXML.getInstance();
+
+	public DataAccessGetEvents(boolean initializeMode) {
+
+		System.out.println("Creating DataAccess instance => isDatabaseLocal: " + c.isDatabaseLocal()
+				+ " getDatabBaseOpenMode: " + c.getDataBaseOpenMode());
+
+		open(initializeMode);
+
+	}
+
+	public DataAccessGetEvents() {
+		this(false);
+	}
+	
+	/**
+	 * This method retrieves from the database the events of a given date
+	 * 
+	 * @param date in which events are retrieved
+	 * @return collection of events
+	 */
+	public Vector<Event> getEvents(Date date) {
+		System.out.println(">> DataAccess: getEvents");
+		Vector<Event> res = new Vector<Event>();
+		TypedQuery<Event> query = db.createQuery("SELECT ev FROM Event ev WHERE ev.eventDate=?1", Event.class);
+		query.setParameter(1, date);
+		List<Event> events = query.getResultList();
+		for (Event ev : events) {
+			System.out.println(ev.toString());
+			res.add(ev);
+		}
+		return res;
+	}
+
+	public void open(boolean initializeMode) {
+
+		System.out.println("Opening DataAccess instance => isDatabaseLocal: " + c.isDatabaseLocal()
+				+ " getDatabBaseOpenMode: " + c.getDataBaseOpenMode());
+
+		String fileName = c.getDbFilename();
+		if (initializeMode) {
+			fileName = fileName + ";drop";
+			System.out.println("Deleting the DataBase");
+		}
+
+		if (c.isDatabaseLocal()) {
+			emf = Persistence.createEntityManagerFactory("objectdb:" + fileName);
+			db = emf.createEntityManager();
+		} else {
+			Map<String, String> properties = new HashMap<String, String>();
+			properties.put("javax.persistence.jdbc.user", c.getUser());
+			properties.put("javax.persistence.jdbc.password", c.getPassword());
+
+			emf = Persistence.createEntityManagerFactory(
+					"objectdb://" + c.getDatabaseNode() + ":" + c.getDatabasePort() + "/" + fileName, properties);
+
+			db = emf.createEntityManager();
+		}
+
+	}
+
+	public void close() {
+		db.close();
+		System.out.println("DataBase closed");
+	}
+}
---
+++ src/test/java/test/dataAccess/GetEventsDAB2Test.java
@@ -0,0 +1,118 @@
+package test.dataAccess;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Vector;
+
+import javax.persistence.PersistenceException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import configuration.UtilDate;
+import dataAccess.DataAccess;
+import domain.Event;
+import exceptions.ObjectAlreadyExistException;
+
+public class GetEventsDAB2Test {
+	
+	static DataAccess sut;
+	static TestDataAccess testDA;
+	
+	int month;
+	int year;
+	
+	Event evTest1;
+	Event evTest2;
+
+	@Before
+	public void initialize() throws ObjectAlreadyExistException {
+		
+		Calendar today = Calendar.getInstance();
+		month = today.get(Calendar.MONTH);
+		month += 1;
+		year = today.get(Calendar.YEAR);
+		if (month == 12) {
+			month = 0;
+			year++;
+		}
+	}
+	
+	// Esta prueba busca una fecha que no tiene eventos, y realiza el getEvents()
+	@Test
+	public void test1() {
+		sut = new DataAccess();
+		testDA = new TestDataAccess();
+		
+		Date date = testDA.getRandomDateWithNoEvents();
+		try {
+			Vector<Event> events = sut.getEvents(date);
+			assertEquals(events.isEmpty(), true);
+		} catch (IllegalArgumentException e) { //Cambiar exception
+			fail("Exception con createQuery() o setParameter()");
+		} catch(IllegalStateException | PersistenceException e) {
+			fail("Exception con getResultList()");
+		} 
+	}
+	
+	// Esta prueba añade un evento a una fecha sin eventos y realiza el getEvents()
+	@Test
+	public void test2() {
+		sut = new DataAccess();
+		testDA = new TestDataAccess();
+		
+		try {
+			evTest1 = sut.createEvent("Pregunta Evento 1", UtilDate.newDate(year, month, 17));
+			
+			@SuppressWarnings("deprecation")
+			Date date = new Date(123, month, 17);
+			Vector<Event> events = sut.getEvents(date);
+			Event returnedEvent1 = events.get(0);
+			assertEquals(evTest1, returnedEvent1);
+		} catch (ObjectAlreadyExistException e) {
+			fail("Error creando el evento.");
+		} catch (IllegalArgumentException e) { //Cambiar exception
+			fail("Exception con createQuery() o setParameter()");
+		} catch(IllegalStateException | PersistenceException e) {
+			fail("Exception con getResultList()");
+		} finally {
+			testDA.open();
+			testDA.removeEvent(evTest1);
+			testDA.close();
+		}
+	}
+	
+	// Esta prueba añade multiples eventos a una fecha sin eventos y realiza el getEvents()
+	@Test
+	public void testGetEventsWithMultipleEvents() {
+		sut = new DataAccess();
+		testDA = new TestDataAccess();
+		
+		try {
+			evTest1 = sut.createEvent("Pregunta Evento 1", UtilDate.newDate(year, month, 17));
+			evTest2 = sut.createEvent("Pregunta Evento 2", UtilDate.newDate(year, month, 17));
+			
+			@SuppressWarnings("deprecation")
+			Date date = new Date(123, month, 17);
+			Vector<Event> events = sut.getEvents(date);
+			Event returnedEvent1 = events.get(0);
+			Event returnedEvent2 = events.get(1);
+			assertEquals(evTest1, returnedEvent1);
+			assertEquals(evTest1, returnedEvent2);
+		} catch (ObjectAlreadyExistException e) {
+			fail("Error creando el evento.");
+		} catch (IllegalArgumentException e) { //Cambiar exception
+			fail("Exception con createQuery() o setParameter()");
+		} catch(IllegalStateException | PersistenceException e) {
+			fail("Exception con getResultList()");
+		} finally {
+			testDA.open();
+			testDA.removeEvent(evTest1);
+			testDA.removeEvent(evTest2);
+			testDA.close();
+		}
+	}
+}
